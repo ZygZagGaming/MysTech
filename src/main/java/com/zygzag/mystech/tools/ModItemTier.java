e8c1a21d2914545ffd2514dea841cefaee7e6a04
ZygZagGaming <zygzaggaming@gmail.com>
2020-10-12 20:52:01 -0400

    Ender tool tier added, including en_us lang edits

---
+++ src/main/java/com/zygzag/mystech/tools/ModItemTier.java
@@ -0,0 +1,60 @@
+package com.zygzag.mystech.tools;
+
+import com.zygzag.mystech.util.RegistryHandler;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+
+import java.util.function.Supplier;
+
+public enum ModItemTier implements IItemTier {
+
+    ENDER(5, 2581, 10.0F, 5.5F, 20, () -> {
+        return Ingredient.fromItems(RegistryHandler.ENDER_SWORD.get());
+    });
+
+    private final int harvestLevel;
+    private final int maxUses;
+    private final float efficiency;
+    private final float attackDamage;
+    private final int enchantability;
+    private final Supplier<Ingredient> repairMaterial;
+
+    ModItemTier(int harvestLevel, int maxUses, float efficiency, float attackDamage, int enchantability, Supplier<Ingredient> repairMaterial) {
+        this.harvestLevel = harvestLevel;
+        this.maxUses = maxUses;
+        this.efficiency = efficiency;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairMaterial = repairMaterial;
+    }
+
+    @Override
+    public int getMaxUses() {
+        return maxUses;
+    }
+
+    @Override
+    public float getEfficiency() {
+        return efficiency;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return attackDamage;
+    }
+
+    @Override
+    public int getHarvestLevel() {
+        return harvestLevel;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial() {
+        return repairMaterial.get();
+    }
+}
--- src/main/java/com/zygzag/mystech/util/RegistryHandler.java
+++ src/main/java/com/zygzag/mystech/util/RegistryHandler.java
@@ -4,8 +4,9 @@ import com.zygzag.mystech.MysTech;
 import com.zygzag.mystech.blocks.BlockItemBase;
 import com.zygzag.mystech.blocks.T1Pillar;
 import com.zygzag.mystech.items.ItemBase;
+import com.zygzag.mystech.tools.ModItemTier;
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -24,6 +25,18 @@ public class RegistryHandler {
     // Items
     public static final RegistryObject<Item> T1_CORE = ITEMS.register("t1_core", ItemBase::new);
     public static final RegistryObject<Item> CORE_FRAME = ITEMS.register("core_frame", ItemBase::new);
+
+    // Tools
+    public static final RegistryObject<SwordItem> ENDER_SWORD = ITEMS.register("ender_sword", () ->
+            new SwordItem(ModItemTier.ENDER, 4, -2.4F, new Item.Properties().group(MysTech.TAB)));
+    public static final RegistryObject<AxeItem> ENDER_AXE = ITEMS.register("ender_axe", () ->
+            new AxeItem(ModItemTier.ENDER, 5, -3.0F, new Item.Properties().group(MysTech.TAB)));
+    public static final RegistryObject<PickaxeItem> ENDER_PICKAXE = ITEMS.register("ender_pickaxe", () ->
+            new PickaxeItem(ModItemTier.ENDER, 2, -2.8F, new Item.Properties().group(MysTech.TAB)));
+    public static final RegistryObject<ShovelItem> ENDER_SHOVEL = ITEMS.register("ender_shovel", () ->
+            new ShovelItem(ModItemTier.ENDER, 2.5F, -3.0F, new Item.Properties().group(MysTech.TAB)));
+    public static final RegistryObject<HoeItem> ENDER_HOE = ITEMS.register("ender_hoe", () ->
+            new HoeItem(ModItemTier.ENDER, 0, 0F, new Item.Properties().group(MysTech.TAB)));
 
     // Blocks
     public static final RegistryObject<Block> T1_INFUSION_PILLAR = BLOCKS.register("t1_pillar", T1Pillar::new);
