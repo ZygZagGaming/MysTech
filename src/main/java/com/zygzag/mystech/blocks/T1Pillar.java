2c4166a32b00839547c8db5eac65de7e3c92ca4f
ZygZagGaming <zygzaggaming@gmail.com>
2020-10-15 15:11:29 -0400

    Testing blockstates

--- src/main/java/com/zygzag/mystech/blocks/T1Pillar.java
+++ src/main/java/com/zygzag/mystech/blocks/T1Pillar.java
@@ -1,8 +1,14 @@
 package com.zygzag.mystech.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.EnumProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
 import net.minecraftforge.common.ToolType;
 
 public class T1Pillar extends Block {
@@ -15,4 +21,13 @@ public class T1Pillar extends Block {
                 .harvestTool(ToolType.PICKAXE)
         );
     }
+
+    EnumProperty<Direction.Axis> axis = BlockStateProperties.AXIS;
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<net.minecraft.block.Block, BlockState> builder) {
+        super.fillStateContainer(builder);
+        builder.add(axis);
+    }
+
 }
