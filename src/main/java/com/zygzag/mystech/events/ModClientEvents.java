45407821c7be7ecbee3ad5982af128054788357e
ZygZagGaming <zygzaggaming@gmail.com>
2020-10-17 23:25:07 -0400

    Added bosss summoning mechanism (no boss yet) and changed alpha version to 8

--- src/main/java/com/zygzag/mystech/events/ModClientEvents.java
+++ src/main/java/com/zygzag/mystech/events/ModClientEvents.java
@@ -3,39 +3,83 @@ package com.zygzag.mystech.events;
 import com.zygzag.mystech.MysTech;
 import com.zygzag.mystech.util.BlockRegistryHandler;
 import com.zygzag.mystech.util.ItemRegistryHandler;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.audio.SoundEngine;
+import net.minecraft.client.audio.SoundHandler;
+import net.minecraft.client.audio.SoundList;
+import net.minecraft.client.audio.SoundSource;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.LightningBoltEntity;
 import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.item.ItemFrameEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.sound.SoundEvent;
+import net.minecraftforge.client.event.sound.SoundLoadEvent;
 import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.lwjgl.system.CallbackI;
+
+import java.util.List;
 
 @Mod.EventBusSubscriber(modid = MysTech.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ModClientEvents {
 
     @SubscribeEvent
     public static void onItemDropped(ItemTossEvent event) {
+    }
 
-        ItemEntity itemEntity = event.getEntityItem();
-        ItemStack item = event.getEntityItem().getItem();
-        LivingEntity player = event.getPlayer();
-        ServerWorld world = (ServerWorld) player.getEntityWorld();
-        LightningBoltEntity bolt = new LightningBoltEntity(EntityType.LIGHTNING_BOLT, world);
-        bolt.forceSetPosition(itemEntity.getPosX(), itemEntity.getPosY(), itemEntity.getPosZ());
-        if (item.getItem() == ItemRegistryHandler.LIGHTNING_BOLT.get() && item.getCount() == 1) {
-            world.summonEntity(bolt);
+    @SubscribeEvent
+    public static void onFirePlaced(PlayerInteractEvent.RightClickBlock event) {
+        if (event.getItemStack().getItem() == Items.FLINT_AND_STEEL || event.getItemStack().getItem() == Items.FIRE_CHARGE) {
+            World world = event.getWorld();
+            BlockPos pos = event.getPos();
+            BlockState state = world.getBlockState(pos);
+            if (state == Blocks.NETHERRACK.getDefaultState()) {
+                AxisAlignedBB bb = new AxisAlignedBB(pos.getX() + 1.5, pos.getY() + 1, pos.getZ() + 1.5, pos.getX() - 1.5, pos.getY() - 1, pos.getZ() - 1.5);
+                List<ItemFrameEntity> list = world.getEntitiesWithinAABB(ItemFrameEntity.class, bb);
+                int i = 0;
+                ItemFrameEntity[] l = new ItemFrameEntity[4];
+                if (list.size() >= 4) {
+                    l[0] = list.get(0);
+                    l[1] = list.get(1);
+                    l[2] = list.get(2);
+                    l[3] = list.get(3);
+                    if (!(world.isRemote())) {
+                        ServerWorld sWorld = (ServerWorld) world;
+                        sWorld.removeEntity(l[0]);
+                        sWorld.removeEntity(l[1]);
+                        sWorld.removeEntity(l[2]);
+                        sWorld.removeEntity(l[3]);
+                        sWorld.removeBlock(pos.add(0, 0, 0), false);
+                        LightningBoltEntity bolt = new LightningBoltEntity(EntityType.LIGHTNING_BOLT, sWorld);
+                        bolt.setPosition(pos.getX(), pos.getY(), pos.getZ());
+                        sWorld.summonEntity(bolt);
+                    }
+                }
+            }
         }
-
     }
 
     @SubscribeEvent
@@ -48,40 +92,42 @@ public class ModClientEvents {
     }
 
     @SubscribeEvent
-    public static void onAttack(LivingAttackEvent event) {
-
-
-        /*
-        if the entity who attacked was a player and was holding a t3 core then it is set to t4
-         */
-        Entity entity = event.getEntityLiving();
-        Entity source = event.getSource().getImmediateSource();
-        assert source != null;
-        if (source.getType() == EntityType.PLAYER) {
-            PlayerEntity player = (PlayerEntity) entity;
-            Item main = player.getHeldItemMainhand().getItem();
-            Item offhand = player.getHeldItemOffhand().getItem();
-            int count = player.getHeldItemMainhand().getCount();
-            int ocount = player.getHeldItemOffhand().getCount();
-            ServerWorld world = (ServerWorld) event.getEntity().world;
+    public static void onLivingAttack(LivingAttackEvent event) {
+        if(!(event.getEntityLiving().getEntityWorld().isRemote())) {
             if (event.getAmount() >= 15) {
-                if (main == ItemRegistryHandler.T3_CORE.get()) {
-                    ItemStack stack = ItemRegistryHandler.T4_CORE.get().getDefaultInstance();
-                    player.setItemStackToSlot(EquipmentSlotType.MAINHAND, stack);
-                    ItemEntity item = new ItemEntity(EntityType.ITEM, world);
-                    stack.setCount(count - 1);
-                    item.setItem(stack);
-                    world.summonEntity(item);
-                }
-                if (offhand == ItemRegistryHandler.T3_CORE.get()) {
-                    ItemStack stack = ItemRegistryHandler.T4_CORE.get().getDefaultInstance();
-                    player.setItemStackToSlot(EquipmentSlotType.OFFHAND, stack);
-                    ItemEntity item = new ItemEntity(EntityType.ITEM, world);
-                    stack.setCount(ocount - 1);
-                    item.setItem(stack);
-                    world.summonEntity(item);
+                ServerWorld world = (ServerWorld) event.getEntity().getEntityWorld();
+                if (event.getSource().getImmediateSource() != null) {
+                    if (event.getSource().getImmediateSource().getType() == EntityType.PLAYER) {
+                        PlayerEntity player = (PlayerEntity) event.getSource().getImmediateSource();
+                        if (player.getHeldItemOffhand().getItem() == ItemRegistryHandler.T3_CORE.get() && player.getHeldItemOffhand().getCount() >= 4 && player.experienceLevel >= 10) {
+                            if (player.getHeldItemOffhand().getCount() > 4) {
+                                ItemEntity item = new ItemEntity(player.getEntityWorld(), player.getPosX(), player.getPosY(), player.getPosZ());
+                                ItemStack itemStack = ItemRegistryHandler.T4_CORE.get().getDefaultInstance();
+                                itemStack.setCount(1);
+                                item.setItem(itemStack);
+                                world.summonEntity(item);
+                            }
+                            ItemStack t3 = ItemRegistryHandler.T3_CORE.get().getDefaultInstance();
+                            t3.setCount(player.getHeldItemOffhand().getCount() - 4);
+                            player.setItemStackToSlot(EquipmentSlotType.OFFHAND, t3);
+                            player.addExperienceLevel(-10);
+                            world.playSound(null, player.getPosX(), player.getPosY(), player.getPosZ(), SoundEvents.AMBIENT_CAVE, SoundCategory.AMBIENT, 1, 1);
+                        }
+                    }
                 }
             }
         }
+    }
+
+    @SubscribeEvent
+    public static void onFireLit(BlockEvent.EntityPlaceEvent event) {
+        /*
+        if fire lit w/ other stuff, summon boss
+         */
+        BlockState block = event.getPlacedBlock();
+        if (block == Blocks.FIRE.getDefaultState()) {
+            
+        }
+
     }
 }
